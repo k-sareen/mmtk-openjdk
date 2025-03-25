3537f08930a88592065f3b94ef099ef73604a720
Kunal Sareen <kunal.sareen@anu.edu.au>
2025-03-25 12:18:44 +0000

    Implement single-threaded GC support

--- mmtk/src/scanning.rs
+++ mmtk/src/scanning.rs
@@ -6,6 +6,8 @@ use mmtk::memory_manager;
 use mmtk::scheduler::WorkBucketStage;
 use mmtk::util::opaque_pointer::*;
 use mmtk::util::{Address, ObjectReference};
+#[cfg(feature = "single_worker")]
+use mmtk::vm::ObjectGraphTraversal;
 use mmtk::vm::{RootsWorkFactory, Scanning, SlotVisitor};
 use mmtk::Mutator;
 use mmtk::MutatorContext;
@@ -44,6 +46,38 @@ pub(crate) fn to_slots_closure<S: Slot, F: RootsWorkFactory<S>>(factory: &mut F)
     }
 }
 
+#[cfg(feature = "single_worker")]
+extern "C" fn object_graph_traversal_report_roots<S: Slot, C: ObjectGraphTraversal<S>>(
+    ptr: *mut Address,
+    length: usize,
+    capacity: usize,
+    traverse_func: *mut libc::c_void,
+) -> NewBuffer {
+    if !ptr.is_null() {
+        let buf = unsafe { Vec::<S>::from_raw_parts(ptr as _, length, capacity) };
+        let traverse_func: &mut C = unsafe { &mut *(traverse_func as *mut C) };
+        traverse_func.report_roots(buf);
+    }
+    let (ptr, _, capacity) = {
+        // TODO: Use Vec::into_raw_parts() when the method is available.
+        use std::mem::ManuallyDrop;
+        let new_vec = Vec::with_capacity(WORK_PACKET_CAPACITY);
+        let mut me = ManuallyDrop::new(new_vec);
+        (me.as_mut_ptr(), me.len(), me.capacity())
+    };
+    NewBuffer { ptr, capacity }
+}
+
+#[cfg(feature = "single_worker")]
+pub(crate) fn object_graph_traversal_to_slots_closure<S: Slot, C: ObjectGraphTraversal<S>>(
+    closure: &mut C,
+) -> SlotsClosure {
+    SlotsClosure {
+        func: object_graph_traversal_report_roots::<S, C>,
+        data: closure as *mut C as *mut libc::c_void,
+    }
+}
+
 impl<const COMPRESSED: bool> Scanning<OpenJDK<COMPRESSED>> for VMScanning {
     fn scan_object<SV: SlotVisitor<OpenJDKSlot<COMPRESSED>>>(
         tls: VMWorkerThread,
@@ -66,6 +100,21 @@ impl<const COMPRESSED: bool> Scanning<OpenJDK<COMPRESSED>> for VMScanning {
         let tls = mutator.get_tls();
         unsafe {
             ((*UPCALLS).scan_roots_in_mutator_thread)(to_slots_closure(&mut factory), tls);
+        }
+    }
+
+    #[cfg(feature = "single_worker")]
+    fn single_threaded_scan_roots_in_mutator_thread(
+        _tls: VMWorkerThread,
+        mutator: &'static mut Mutator<OpenJDK<COMPRESSED>>,
+        mut closure: impl ObjectGraphTraversal<OpenJDKSlot<COMPRESSED>>,
+    ) {
+        let tls = mutator.get_tls();
+        unsafe {
+            ((*UPCALLS).scan_roots_in_mutator_thread)(
+                object_graph_traversal_to_slots_closure(&mut closure),
+                tls,
+            );
         }
     }
 
@@ -93,6 +142,48 @@ impl<const COMPRESSED: bool> Scanning<OpenJDK<COMPRESSED>> for VMScanning {
         );
     }
 
+    #[cfg(feature = "single_worker")]
+    fn single_threaded_scan_vm_specific_roots(
+        _tls: VMWorkerThread,
+        mut closure: impl ObjectGraphTraversal<OpenJDKSlot<COMPRESSED>>,
+    ) {
+        unsafe {
+            ((*UPCALLS).scan_universe_roots)(object_graph_traversal_to_slots_closure(&mut closure));
+            ((*UPCALLS).scan_jni_handle_roots)(object_graph_traversal_to_slots_closure(
+                &mut closure,
+            ));
+            ((*UPCALLS).scan_object_synchronizer_roots)(object_graph_traversal_to_slots_closure(
+                &mut closure,
+            ));
+            ((*UPCALLS).scan_management_roots)(object_graph_traversal_to_slots_closure(
+                &mut closure,
+            ));
+            ((*UPCALLS).scan_jvmti_export_roots)(object_graph_traversal_to_slots_closure(
+                &mut closure,
+            ));
+            ((*UPCALLS).scan_aot_loader_roots)(object_graph_traversal_to_slots_closure(
+                &mut closure,
+            ));
+            ((*UPCALLS).scan_system_dictionary_roots)(object_graph_traversal_to_slots_closure(
+                &mut closure,
+            ));
+            ((*UPCALLS).scan_string_table_roots)(object_graph_traversal_to_slots_closure(
+                &mut closure,
+            ));
+            ((*UPCALLS).scan_class_loader_data_graph_roots)(
+                object_graph_traversal_to_slots_closure(&mut closure),
+            );
+            ((*UPCALLS).scan_weak_processor_roots)(object_graph_traversal_to_slots_closure(
+                &mut closure,
+            ));
+            ((*UPCALLS).scan_vm_thread_roots)(object_graph_traversal_to_slots_closure(
+                &mut closure,
+            ));
+        }
+
+        single_threaded_scan_code_cache_roots::<COMPRESSED>(&mut closure);
+    }
+
     fn supports_return_barrier() -> bool {
         unimplemented!()
     }
@@ -101,5 +192,56 @@ impl<const COMPRESSED: bool> Scanning<OpenJDK<COMPRESSED>> for VMScanning {
         unsafe {
             ((*UPCALLS).prepare_for_roots_re_scanning)();
         }
+    }
+}
+
+#[cfg(feature = "single_worker")]
+fn single_threaded_scan_code_cache_roots<const COMPRESSED: bool>(
+    closure: &mut impl ObjectGraphTraversal<OpenJDKSlot<COMPRESSED>>,
+) {
+    let is_current_gc_nursery = crate::singleton::<COMPRESSED>()
+        .get_plan()
+        .generational()
+        .is_some_and(|gen| gen.is_current_gc_nursery());
+
+    let mut slots = Vec::with_capacity(WORK_PACKET_CAPACITY);
+
+    let mut nursery_slots = 0;
+    let mut mature_slots = 0;
+
+    let mut add_roots = |roots: &[Address]| {
+        for root in roots {
+            slots.push(OpenJDKSlot::<COMPRESSED>::from(*root));
+            if slots.len() >= WORK_PACKET_CAPACITY {
+                closure.report_roots(std::mem::take(&mut slots));
+            }
+        }
+    };
+
+    {
+        let mut mature = crate::MATURE_CODE_CACHE_ROOTS.lock().unwrap();
+
+        // Only scan mature roots in full-heap collections.
+        if !is_current_gc_nursery {
+            for roots in mature.values() {
+                mature_slots += roots.len();
+                add_roots(roots);
+            }
+        }
+
+        {
+            let mut nursery = crate::NURSERY_CODE_CACHE_ROOTS.lock().unwrap();
+            for (key, roots) in nursery.drain() {
+                nursery_slots += roots.len();
+                add_roots(&roots);
+                mature.insert(key, roots);
+            }
+        }
+    }
+
+    probe!(mmtk_openjdk, code_cache_roots, nursery_slots, mature_slots);
+
+    if !slots.is_empty() {
+        closure.report_roots(slots);
     }
 }
