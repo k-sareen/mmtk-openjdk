4a71b9f034a74d507584c675694744120dbc099b
Kunal Sareen <kunal.sareen@anu.edu.au>
2026-05-11 13:36:16 +0000

    Merge for Compressor and OnePass

--- mmtk/src/slots.rs
+++ mmtk/src/slots.rs
@@ -205,6 +205,10 @@ impl<const COMPRESSED: bool> Slot for OpenJDKSlot<COMPRESSED> {
             unsafe { self.addr.store(object) }
         }
     }
+
+    fn as_address(&self) -> Address {
+        self.addr
+    }
 }
 
 /// A range of OpenJDKSlot, usually used for arrays.
